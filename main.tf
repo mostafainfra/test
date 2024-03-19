c12e229e3cef3b1395945785a15eba64147b6d47
mostafainfra <mostafa.infra@hotmail.com>
2024-03-19 22:43:52 +0000

    testMMM

--- main.tf
+++ main.tf
@@ -1,8 +1,37 @@
-provider "azurerm" {
-  features {}
+resource "azurerm_resource_group" "example" {
+  name     = "example-resourcesmmm"
+  location = "West Europe"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-rgM"
-  location = "West Europe"
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplanMMMMMM"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-app-serviceMMMMMMMM"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
 }
