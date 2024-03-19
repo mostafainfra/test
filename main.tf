f845d68f3facd08d55495e2170866fc9fff19129
mostafainfra <mostafa.infra@hotmail.com>
2024-03-19 22:58:45 +0000

    testM

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplanMMMMMM1112"
+  name                = "example-appserviceplanMMMMMM11123"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
@@ -19,7 +19,7 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_app_service" "example" {
-  name                = "example-app-serviceMMMMMMMM111112"
+  name                = "example-app-serviceMMMMMMMM1111123"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
