92e5a970973cd57f994af77b0c52c236e7246e97
mostafainfra <mostafa.infra@hotmail.com>
2024-03-22 16:00:42 +0000

    testMM

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resourcesmmm1112"
+  name     = "example-resourcesmm"
   location = "West Europe"
 }
 
 resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplanMMMMMM11123"
+  name                = "example-appserviceplanMMMM"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
